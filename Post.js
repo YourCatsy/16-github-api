04f90a6fce8bae97d18efbdb84502360be49e122
Catherine_AMF <catherineamf@gmail.com>
2022-02-10 18:34:42 +0200

    replace properties from scrpt.js

--- Post.js
+++ Post.js
@@ -1,13 +1,17 @@
 'use strict';
 
 class Post {
-    static getList(url) {
+    static LOGIN_PLACEHOLDER = '{{login}}';
+    static GET_USER_URL = `https://api.github.com/users/${Post.LOGIN_PLACEHOLDER}`;
+    
+    static getList(userName) {
+        const url = Post.GET_USER_URL.replace(Post.LOGIN_PLACEHOLDER, userName);
         return fetch(url, { method: 'GET' })
             .then((response) => {
                 if (response.ok) {
                     return response.json();
                 } else {
-                    throw Error(response.status);
+                    throw Error(NOT_FOUND_MESSAGE);
                 }
             });
     }
